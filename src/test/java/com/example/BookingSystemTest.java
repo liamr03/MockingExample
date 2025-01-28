6c52ae8bf2784046d5d8c73a5c118efaf9ef2048
liamr03 <liamr03@hotmail.com>
2025-01-28 14:06:37 +0100

    Create BookingSystemTest

---
+++ src/test/java/com/example/BookingSystemTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BookingSystemTest {
+
+    @Test
+    void bookRoom() {
+    }
+
+    @Test
+    void getAvailableRooms() {
+    }
+
+    @Test
+    void cancelBooking() {
+    }
+}
