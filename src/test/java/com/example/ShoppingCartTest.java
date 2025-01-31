4fcf4dc91f4a1d47e1d13d294cc523fade385738
liamr03 <liamr03@hotmail.com>
2025-01-31 15:29:03 +0100

    Added Update, Calculate and Exception For Null Item Tests For Shopping Cart

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -21,11 +21,45 @@ public class ShoppingCartTest {
     }
 
     @Test
+    @DisplayName("Should Remove Item From Cart")
     void shouldRemoveItemFromCart() {
         Item item = new Item("item1", 10.0);
-        cart.addItem(item);
-        cart.removeItem(item);
+        cart.addToCart(item);
+        cart.removeFromCart(item);
 
         assertThat(cart.getItems()).doesNotContain(item);
+    }
+
+    @Test
+    @DisplayName("Should Calculate Total Price of Items In Cart")
+    void shouldCalculateTotalPrice() {
+        Item item1 = new Item("item1", 10.0);
+        Item item2 = new Item("item2", 15.0);
+        cart.addItem(item1);
+        cart.addItem(item2);
+
+        double totalPrice = cart.calculateTotalPrice();
+
+        assertThat(totalPrice).isEqualTo(25.0);
+    }
+
+    @Test
+    @DisplayName("Should Update Item Quantity")
+    void shouldUpdateItemQuantity() {
+        Item item = new Item("item1", 10.0);
+        cart.addItem(item);
+        cart.updateQuantity(item, 5);
+
+        int quantity = cart.getItemQuantity(item);
+
+        assertThat(quantity).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("Should Throw Exception For Null Items")
+    void shouldThrowExceptionForNullItem() {
+        assertThatThrownBy(() -> cart.addItem(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Item cannot be null");
     }
 }
