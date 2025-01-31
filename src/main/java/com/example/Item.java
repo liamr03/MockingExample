424f49592af4d749e4e19f9ef97d31bd82fa30ec
liamr03 <liamr03@hotmail.com>
2025-01-31 15:38:17 +0100

    Added get methods for name and price so i can use them in shoppingcart class

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -1,4 +1,32 @@
 package com.example;
 
 public class Item {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return name.equals(item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
