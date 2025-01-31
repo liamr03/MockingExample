095a7531248644a18d4b3781f3bbb5830b0f537f
liamr03 <liamr03@hotmail.com>
2025-01-31 15:13:30 +0100

    Added Add/Remove Tests For Shopping Cart

--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,8 +1,31 @@
 package com.example;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ShoppingCartTest {
 
+    private ShoppingCart cart;
+
     void setUp() {
-        
+        cart = new ShoppingCart();
+    }
+
+    @Test
+    @DisplayName("Should Add Item To Cart")
+    void shouldAddItemToCart () {
+        Item item = new Item("item1", 10.0);
+        cart.addItem(item);
+
+        assertThat(cart.getItems()).contains(item);
+    }
+
+    @Test
+    void shouldRemoveItemFromCart() {
+        Item item = new Item("item1", 10.0);
+        cart.addItem(item);
+        cart.removeItem(item);
+
+        assertThat(cart.getItems()).doesNotContain(item);
     }
 }
