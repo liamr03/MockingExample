8f711f998d4026847a79d413ce74a2a1f91ffbcd
liamr03 <liamr03@hotmail.com>
2025-01-31 15:57:03 +0100

    Added setPrice

--- src/main/java/com/example/Item.java
+++ src/main/java/com/example/Item.java
@@ -17,6 +17,10 @@ public class Item {
         return price;
     }
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
